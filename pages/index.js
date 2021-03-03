b68d8a763c36c5a22ab68bb74cefaca478429a80
razvan-soare <soare.razvan94@gmail.com>
2021-03-03 14:20:44 +0000

    Set initial text

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,15 @@ import EditableText from "../src/EditableText";
 export default function Home() {
   return (
     <div className="container">
-      <EditableText fileUrl="pages/index.js" id="title-1"><h1 key="0">
-  Hi there 👋 We can edit our content wow  🎉
-</h1><p key="1">
-  aasend your email @
-</p></EditableText>
+      <EditableText fileUrl="pages/index.js" id="title-1"><h1>
+  <span style={{
+      fontSize: 'xx-large'
+    }}
+  >
+    Hi, I am Razvan 
+  </span>
+  👋
+</h1></EditableText>
     </div>
   );
 }
